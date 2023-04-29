259991b686a261cf255ce7f8a0df9f46670d5879
53911453+brianpavon@users.noreply.github.com <53911453+brianpavon@users.noreply.github.com>
2023-04-29 00:14:23 -0300

    inicio alarmaApp

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'alarmaApp',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
