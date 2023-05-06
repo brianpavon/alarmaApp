db30e1f1df433cd0999744b9da7e2a968fdae4aa
53911453+brianpavon@users.noreply.github.com <53911453+brianpavon@users.noreply.github.com>
2023-05-06 00:31:34 -0300

    ok login rapido

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  
+  formRegistro:FormGroup;
 
-  constructor() { }
+  constructor(private auth : AuthService,private fb : FormBuilder) {
+    this.formRegistro = this.fb.group(
+      {
+        'email':['',[Validators.required,Validators.email]],
+        'pass':['',[Validators.required,Validators.minLength(6)]]
+      }
+    );
+  }
 
   ngOnInit() {
+  }
+
+  async login(){
+    try {
+      await this.auth.login(this.formRegistro.value.email,this.formRegistro.value.pass);
+      this.formRegistro.reset();
+    } catch (error) {
+      console.log(error);      
+    }
+  }
+
+  logueoRapido(email:string,pass:string){
+    this.formRegistro.controls['email'].patchValue(email);
+    this.formRegistro.controls['pass'].patchValue(pass);
   }
 
 }
